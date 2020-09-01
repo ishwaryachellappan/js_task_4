49a6d3130a001c598be61bcf6327769a82b89966
Ishwarya <ishwaryathendral@gmail.com>
2020-09-01 18:43:38 +0530

    [IMP] This repository contains async await and promises

--- async-await.js
+++ async-await.js
@@ -1,17 +1,17 @@
+const randomNumber1 = new Promise((resolve, reject) => {
 
-/const randomNumber1 = new Promise((resolve, reject) => {
-
-// Math class is used to generate the random number.
 	resolve(Math.random( ))
 })
 const randomNumber2 = new Promise((resolve, reject) => {
 	resolve(Math.random( ))
 })
 Promise.all([randomNumber1, randomNumber2])
-
-
 .then((messages) => {
 	console.log(messages)
 })
 
- 
+if((randomNumber1+randomNumber2) <= 0) {
+        console.log("User Logged in successfully!");
+    } else  {
+        console.log("Oops!try again");
+    }
