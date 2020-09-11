0ac306ea80623c9f4f13ada21b1f91a714aae361
Ishwarya <ishwaryathendral@gmail.com>
2020-09-11 18:45:20 +0530

    [IMP] This repo consists of async await and promises in javascript

--- promise.js
+++ promise.js
@@ -1,13 +1,13 @@
-
+//declaring a promise
 
 let p = new Promise((resolve, reject) => {
 
-
+//declaring the prime number array
 const primeNumber = [2,3,5,7,11,13];
-
+//getting millisecond through a package
 var today = new Date();
 var milliseconds = today.getUTCMilliseconds();
-
+//using conditional statement
 if ( primeNumber[4] % milliseconds ==0)
 {
 	resolve(' Sucess !')
